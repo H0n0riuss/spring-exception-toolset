180c88fef6ca86795270049be976eec03faf3aea
H0n0riuss <joscha.reichenberg@web.de>
2023-08-25 21:10:30 +0200

    :sparkles: handle HttpServletRequests

--- src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/BaseRestExceptionHandler.java
+++ src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/BaseRestExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.github.honoriuss.springexceptiontoolset.exceptions;
 
 import io.github.honoriuss.springexceptiontoolset.exceptions.models.ApiError;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,12 @@ public class BaseRestExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleException(Exception ex, WebRequest request) {
         ApiError apiError = new ApiError(request.getDescription(false), ex.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception ex, HttpServletRequest request) {
+        ApiError apiError = new ApiError(request.getRequestURI(), ex.getMessage());
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/models/ApiError.java
+++ src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/models/ApiError.java
@@ -1,5 +1,11 @@
 package io.github.honoriuss.springexceptiontoolset.exceptions.models;
 
-public record ApiError(String uri,
-                       String message) {
+public class ApiError {
+    public String uri;
+    public String message;
+
+    public ApiError(String uri, String message) {
+        this.uri = uri;
+        this.message = message;
+    }
 }
