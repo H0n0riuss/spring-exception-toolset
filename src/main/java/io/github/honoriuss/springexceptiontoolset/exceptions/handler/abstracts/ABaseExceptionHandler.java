5537f300144706d5ae09f6b507ab7e977761a1e3
H0n0riuss <joscha.reichenberg@web.de>
2023-08-27 21:31:44 +0200

    :recycle: ABaseExceptionHandler

---
+++ src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/handler/abstracts/ABaseExceptionHandler.java
@@ -0,0 +1,29 @@
+package io.github.honoriuss.springexceptiontoolset.exceptions.handler.abstracts;
+
+import io.github.honoriuss.springexceptiontoolset.exceptions.models.ApiErrorModel;
+import io.github.honoriuss.springexceptiontoolset.exceptions.services.BaseExceptionService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+public abstract class ABaseExceptionHandler {
+    @Value("${exception-toolset.api-error.http-status-code:400}")
+    private int httpStatusCode;
+    private final BaseExceptionService baseExceptionService;
+
+    public ABaseExceptionHandler(BaseExceptionService baseExceptionService) {
+        this.baseExceptionService = baseExceptionService;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorModel> handleException(Exception ex,
+                                                         WebRequest webRequest,
+                                                         HttpServletRequest servletRequest) {
+        return new ResponseEntity<>(
+                baseExceptionService.createApiErrorMessage(ex.getMessage(), webRequest, servletRequest),
+                HttpStatusCode.valueOf(httpStatusCode));
+    }
+}
