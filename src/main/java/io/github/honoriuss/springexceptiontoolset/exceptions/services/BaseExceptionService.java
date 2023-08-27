efd0634a2684c6bd2432a93af89811738a7ecc64
H0n0riuss <joscha.reichenberg@web.de>
2023-08-27 20:37:54 +0200

    :sparkles: adding include-client-info in application.yml

--- src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/services/BaseExceptionService.java
+++ src/main/java/io/github/honoriuss/springexceptiontoolset/exceptions/services/BaseExceptionService.java
@@ -5,12 +5,15 @@ import io.github.honoriuss.springexceptiontoolset.exceptions.models.ApiUrlErrorM
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.context.request.WebRequest;
 
 @Service
 public class BaseExceptionService {
     @Value("${exception-toolset.api-error.include-url:true}")
     private boolean showUrl;
+    @Value("${exception-toolset.api-error.include-client-info:false}")
+    private boolean includeClientInfo;
 
     public ApiErrorModel createApiErrorMessage(String message, WebRequest webRequest, HttpServletRequest servletRequest) {
         return showUrl
@@ -24,8 +27,9 @@ public class BaseExceptionService {
 
     private String getUrl(WebRequest webRequest, HttpServletRequest servletRequest) {
         if (webRequest != null) {
-            return webRequest.getDescription(false);
+            return webRequest.getDescription(includeClientInfo);
         }
+        Assert.notNull(servletRequest, "webRequest and HttpServletRequest are null.");
         return servletRequest.getRequestURI();
     }
 }
